8230ca9537db462a41e4114d0c95a0a6f399023c
Maanas Arora <maanasarora23@gmail.com>
2026-05-14 02:58:26 -0400

    BUG: Add sentinel guard for out-of-bound argsorts

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -2931,6 +2931,10 @@ class TestMethods:
 
         idx = np.argsort(a, stable=stable, descending=descending, axis=-1)
         sorted_a = a[idx]
+        if np.issubdtype(a.dtype, np.object_):
+            # cast to float for comparison, as object does not support isnan
+            sorted_a = sorted_a.astype(float)
+
         diff_sorted_a = np.diff(sorted_a[:-11])
         if descending:
             diff_sorted_a = -diff_sorted_a
@@ -2949,6 +2953,10 @@ class TestMethods:
 
         idx = np.argsort(a_randomized, stable=stable, descending=descending, axis=-1)
         sorted_a = a_randomized[idx]
+        if np.issubdtype(a.dtype, np.object_):
+            # cast to float for comparison, as object does not support isnan
+            sorted_a = sorted_a.astype(float)
+
         diff_sorted_a = np.diff(sorted_a[:-11])
         if descending:
             diff_sorted_a = -diff_sorted_a
